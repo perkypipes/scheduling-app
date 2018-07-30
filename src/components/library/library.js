f9e54e7b1cf6db6a144f0b50d6db0a87a8be4b33
Piper Pillar <piper.pillar@gmail.com>
2018-07-30 17:40:19 +0000

    implemented mapstatetoprops and mapped over data to return the amout of course component instances

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from '../../actions';
+
 import LibraryCourse from './libraryCourse';
 
 class Library extends Component {
-    
-    componentDidMount () {
+
+    componentWillMount() {
         this.props.fetchCourses()
     }
     
-    render () {
+    renderCourses() {
+        const data = this.props.courses
+        
+        return data.map((course, index) => {
+            return <LibraryCourse key={index}/>
+        })
+    }
+
+    render() {
+        console.log(this.props.courses);
         return (
             <div className="library">
                 <h1 className="library__title">Course Library</h1>
-                <LibraryCourse/>
-                <LibraryCourse/>
-                <LibraryCourse/>
+                { this.renderCourses() }
             </div>
-            );
+        )
     }
 }
 
-export default connect(null, actions)(Library);
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
