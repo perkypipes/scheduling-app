51d98670a3da435154ac2076452ac547195c8c7e
Piper Pillar <piper.pillar@gmail.com>
2018-08-06 17:50:36 +0000

    line styles and arrow animation

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class Arrow extends Component {
-    render() {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      status: false
+    }
+  }
+
+  toggleArrow = function() {
+    if(this.state.status) {
+      //closes it
+      document.getElementById('arrow').classlist.remove('arrow-closed');
+
+    } else {
+      //open
+      document.getElementById('arrow').classlist.add('arrow-closed');
+    }
+
+    this.setState({ status: !this.state.status })
+  }.bind(this);
+
+  render() {
         return (
-            <div className={`${this.props.className} arrow`}>
-            
-            </div>
-            )
+            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+        )
     }
 }
 
